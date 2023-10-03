e82da58fbdfc7486c2992fc3cd437f437e4162e2
Student3457 <146750082+Student3457@users.noreply.github.com>
2023-10-02 23:45:31 -0400

    Imcomplete

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+
 
 public class UniqueWords
 {
@@ -7,20 +7,36 @@ public class UniqueWords
 		@param list ArrayList of strings to be examined
 		@return number of unique strings in the list
    */
-   public static int countUnique(ArrayList<String> list)
-   {
-	  int count = 0;
-	  
-      for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
-      }
-	  return count;
-   }
+   public static int countUnique(ArrayList<String> list)    {
+		 
+	   
+	    	  List<Boolean> list1=new ArrayList<Boolean>(Arrays.asList(new Boolean[10]));
+	    	  Collections.fill(list1, Boolean.TRUE);
+	    	  
+	    	  boolean[] array = new boolean[list.size()];
+	          int j = 0;
+	          int i = 0;
+	          int count = 0;
+	          for (i = 0; i < array.length; i++) {
+	              if (array[i]) {
+	                  count++;
+	                  for (j = i + 1; j < array.length; j++) {
+	                      if (array.length[i] == array.length[j]) {
+	                          array[j] = true;
+	                          count--;
+	                      }
+	                  }
+	              }
+	          }
+	          return count;
+	      }
+	    	  
+		
+   
+   
 
-   public static void main(String[] args)
+
+public static void main(String[] args)
    {
       ArrayList <String> words = new ArrayList<>();
       words.add("apple");
@@ -32,6 +48,9 @@ public class UniqueWords
 	  words.add("moon");
    
 	  int unique = countUnique(words);
+	
+	
+	 
       System.out.println(words + " has " + unique + " unique words");
    }
 }
